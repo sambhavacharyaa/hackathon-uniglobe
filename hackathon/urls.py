448067ea4113d262d314e30957512bc448c58dad
Sambhav Acharya <work.sambhavacharya@gmail.com>
2026-08-08 08:21:39 +0545

    created static files

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -14,9 +14,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import static
 from django.contrib import admin
 from django.urls import path
+
+from hackathon import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# URL prefix for uploaded media files
+
+MEDIA_URL = "/media/"
+
+# Directory where uploaded files are stored
+
+MEDIA_ROOT = settings.BASE_DIR / "media"
+
+# URL prefix for static files
+
+STATIC_URL = "static/"
