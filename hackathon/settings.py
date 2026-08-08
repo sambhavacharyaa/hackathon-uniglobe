e52217f64f301da1906c2abc6e74752572d1b581
Sambhav Acharya <work.sambhavacharya@gmail.com>
2026-08-08 08:14:36 +0545

    Addd templates and other folders

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'core',
     'django.contrib.staticfiles',
 ]
 
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'hackathon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
